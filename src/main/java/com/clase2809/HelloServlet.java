3cc96b91380e9d9e6af814a7813006ccbac701d9
amunlozb <amunlozb@g.educaand.es>
2023-09-28 13:50:34 +0200

    asd

--- src/main/java/com/clase2809/HelloServlet.java
+++ src/main/java/com/clase2809/HelloServlet.java
@@ -1,26 +1,31 @@
 package com.clase2809;
 
 import java.io.*;
+import java.util.List;
 
+import com.clase2809.model.Persona;
+import com.clase2809.servicio.PersonaServicio;
+import com.clase2809.servicio.PersonaServicioImpl;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    PersonaServicio servicio = new PersonaServicioImpl();
 
     public void init() {
-        message = "Hello World!";
+        message = "Do get";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        System.out.println("do get reached");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        List<Persona> personas = servicio.getAllPersonas();
+        request.setAttribute("PERSONAS", personas);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("");
     }
 
     public void destroy() {
---
+++ src/main/webapp/listaPersona.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: usuario_mañana
+  Date: 28/09/2023
+  Time: 13:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Lista de Personas</title>
+</head>
+<body>
+  <%
+
+  %>
+</body>
+</html>
